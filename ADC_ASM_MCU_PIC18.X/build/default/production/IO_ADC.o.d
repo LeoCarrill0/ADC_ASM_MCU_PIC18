698ec3df8eb0369d8ebd9c5634a805b11ba8d5ef
LeoCarrill0 <89870928+LeoCarrill0@users.noreply.github.com>
2021-09-01 11:15:08 -0600

    21

---
+++ ADC_ASM_MCU_PIC18.X/build/default/production/IO_ADC.o.d
@@ -0,0 +1,4 @@
+build/default/production/IO_ADC.o:  \
+	F:/Electronics/ASM/PIC18F4550/ADC.X/IO_ADC.asm  \
+	F:/Electronics/ASM/PIC18F4550/ADC.X/Retardo.inc  \
+
---
+++ ADC_ASM_MCU_PIC18.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ADC_ASM_MCU_PIC18.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ADC_ASM_MCU_PIC18.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=adcasmmcupic18.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/adcasmmcupic18.x.tar
